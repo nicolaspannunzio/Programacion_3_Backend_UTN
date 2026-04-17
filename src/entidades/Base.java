4da8e3a775831e1a770d0fd9c32d232bf74a37e9
Nicolás Pannunzio <nicolas.a.pannunzio@email.com>
2026-04-16 21:19:05 -0300

    estructura inicial, enums y clase base

---
+++ src/entidades/Base.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+/**
+ *
+ * @author nicol
+ */
+
+import java.time.LocalDateTime;
+
+public abstract class Base {
+    
+    protected Long id;
+    protected boolean eliminado;
+    protected LocalDateTime createdAt;
+
+    // Constructor vacío
+    public Base() {
+    }
+
+    // Constructor con parámetros
+    public Base(Long id, boolean eliminado, LocalDateTime createdAt) {
+        this.id = id;
+        this.eliminado = eliminado;
+        this.createdAt = createdAt;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
